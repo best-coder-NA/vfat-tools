11c153144fb5fb9b183e682b54e41cf339da8b62
Kevin McIntyre <kebin70@gmail.com>
2021-04-21 02:03:47 +0000

    okay good

--- src/static/js/delegate.js
+++ src/static/js/delegate.js
@@ -20,6 +20,7 @@ const prettyNumber = (n, fixed) => {
 
 async function main() {
 
+
   const app = await init_ethers();
 
   let walletAddres = `${app.YOUR_ADDRESS}`;
@@ -30,21 +31,57 @@ async function main() {
   let snowballmultisig = '0x294aB3200ef36200db84C4128b7f1b4eec71E38a';
   let root = '0x0000000000000000000000000000000000000000';
 
-  let png_contract = new ethers.Contract('0x60781C2586D68229fde47564546784ab3fACA982', YAM_TOKEN_ABI, signer)
+  let png_vote = new ethers.Contract('0x60781C2586D68229fde47564546784ab3fACA982', YAM_TOKEN_ABI, signer)
 
-  png_contract.getCurrentVotes(snowballmultisig).then(results => {
+  png_vote.getCurrentVotes(snowballmultisig).then(results => {
     let vc = results / 1e18
     let sr = 1000000 - vc
     $('#votecount').html(prettyNumber(vc));
     $('#stillrequire').html(prettyNumber(sr))
   })
 
-  png_contract.delegates(app.YOUR_ADDRESS).then(results => {
+  let png_token = new ethers.Contract('0x60781C2586D68229fde47564546784ab3fACA982', ERC20_ABI, signer)
+
+  png_token.balanceOf(app.YOUR_ADDRESS).then(results => {
+    console.log('results:', results)
+    let bo = results / 1e18
+    if ( bo === 0 ) {
+      $('#yourcontribution').html(`${prettyNumber(bo)} PNGs of support is still support`)
+    } else {
+      $('#yourcontribution').html(`We'd appreciate your ${prettyNumber(bo)} PNGs of support`)
+    }    
+  })
+
+  Promise.all([fetch('https://x-api.snowball.network/snob/price').then(res => res.text()).then(price => {
+    console.log('price:', price)
+    return parseFloat(price)
+  }),fetch('https://x-api.snowball.network/snob/circulating').then(res => res.text()).then(circulating => {
+    console.log('circulating:', circulating)
+    return parseFloat(circulating)
+  }),fetch('https://x-api.snowball.network/snob/harvest').then(res => res.text()).then(harvest => {
+    console.log('harvest:', harvest)
+    return parseFloat(harvest)
+  })]).then(res => {
+    console.log('results:', res)
+    $('#facts').html(`
+    <dl>
+    <dt style="color:#fff">Snowball By Books</dt>
+    <dd style="white-space: nowrap">$${prettyNumber(res[0], 2)} Price</dd>
+    <dd style="white-space: nowrap">${prettyNumber(res[1], 0)} Circulating Supply</dd>
+    <dd style="white-space: nowrap">$${prettyNumber(res[1] * res[0], 0)} Market Cap</dd>
+    <dd style="white-space: nowrap">$${prettyNumber(res[2], 2)} Reinvestments</dd>
+    </dl>
+    `);
+  }).catch(err => {
+    console.log('fact error:', err);
+  })
+  
+  png_vote.delegates(app.YOUR_ADDRESS).then(results => {
     if ( results === snowballmultisig ) {
       $('#undelegate').show();
       $('button.undelegate').on('click', (e) => {
         e.target.disabled = true;
-        png_contract.delegate(root).then( result => {
+        png_vote.delegate(root).then( result => {
           setTimeout( () => {
             document.location.reload()
           }, 5000)          
@@ -60,7 +97,7 @@ async function main() {
         e.target.disabled = true;
         $('#effects').html(`<audio controls autoplay><source src="https://x-api.snowball.network/dex/delegate.mp3" type="audio/mpeg"></audio>`)
 
-        png_contract.delegate(snowballmultisig).then( result => {
+        png_vote.delegate(snowballmultisig).then( result => {
           setTimeout( () => {
             document.location.reload()
           }, 5000)          
